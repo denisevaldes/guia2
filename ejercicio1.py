e4941faa83d12eae85a6e92dad90887eac10278f
denisevaldes <cadelige@gmail.com>
2021-09-10 22:34:25 -0300

    update

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,9 +1,13 @@
-#Dada un String que se forman de manera aleatoria, devuelva una nueva lista cuyo contenido sea igual
-#a la original pero de modo invertida.
+# Denise valdés
+# Ejercicio 1
+# Dada un String que se forman de manera aleatoria, devuelva una nueva lista cuyo contenido sea igual
+# a la original pero de modo invertida.
 
 texto = "estas como hola"
 separar_texto = texto.split()
-
+# se crea for y se ingresa
 for i in range(len(separar_texto)-1,-1,-1):
-    reversa = separar_texto[i]
+    # se imprime el texto en reversa y se añade un espacio
+    # para que las palabras se escriban hacia el lado
+    print(separar_texto[i], end = " ")
     
