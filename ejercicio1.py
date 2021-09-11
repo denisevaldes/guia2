34031e711a9b8691e8a2571f4a4d04311b0284bf
denisevaldes <cadelige@gmail.com>
2021-09-10 22:43:35 -0300

    update

--- ejercicio1.py
+++ ejercicio1.py
@@ -5,7 +5,7 @@
 
 texto = "estas como hola"
 separar_texto = texto.split()
-# se crea for y se ingresa
+# se crea for y se agrega -1 para ir decreciendo 
 for i in range(len(separar_texto)-1,-1,-1):
     # se imprime el texto en reversa y se añade un espacio
     # para que las palabras se escriban hacia el lado
